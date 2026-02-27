e25824ea2dd8c1c445245a772b6289026bf204cf
No-Jyun <hj010410@gmail.com>
2026-02-27 15:39:25 +0900

    A* 알고리즘 FindPath 구현 및 데모 테스트
    - FindPath 구현 완료.
    - 길찾기 테스트 코드 작성 (Main.cpp)

--- AStartPathFinding/Navigation/AStar.cpp
+++ AStartPathFinding/Navigation/AStar.cpp
@@ -27,7 +27,161 @@ AStar::~AStar()
 
 std::vector<Node*> AStar::FindPath(Node* startNode, Node* goalNode, std::vector<std::vector<int>>& grid)
 {
-    return std::vector<Node*>();
+    // 시작 / 목표 노드 저장
+    this->startNode = startNode;
+    this->goalNode = goalNode;
+
+    // 예외 처리
+    if (!this->startNode || !this->goalNode || grid.empty() || grid[0].empty())
+    {
+        // 빈 경로 반환 (오류)
+        //return std::vector<Node*>();
+        return {};
+    }
+
+    // 시작 노드를 열린 리스트에 추가 및 탐색 시작
+    openList.emplace_back(this->startNode);
+
+    // 대각선 비용 상수
+    const float diagonalcost = 1.41421345f;
+
+        // 비용 계산에 사용할 변수 값 설정
+    std::vector<Direction> directions =
+    {
+        // 하상우좌 이동
+        { 0, 1, 1.0f }, { 0, -1, 1.0f }, { 1, 0, 1.0f }, { -1, 0, 1.0f },
+        // 대각선 이동
+        { 1, 1, diagonalcost }, { 1, -1, diagonalcost }, 
+        { -1, 1, diagonalcost }, { -1, -1, diagonalcost }
+    };
+
+    // 탐색 가능한 위치가 있으면 계속 진행
+    while (!openList.empty())
+    {
+        // 현재 열린 리스트에 있는 노드 중 fCost가 가장 낮은 노드 검색
+        Node* lowestNode = openList[0];
+
+        // 가장 비용이 작은 노드 검색 (선형 탐색)
+        for (Node* const node : openList)
+        {
+            if (node->fCost < lowestNode->fCost)
+            {
+                lowestNode = node;
+            }
+        }
+
+        // fCost가 가장 낮은 노드로 설정
+        Node* currentNode = lowestNode;
+
+        // 현재 노드가 목표 노드인지 확인
+        if (IsDestination(currentNode))
+        {
+            // 경로 반환 후 종료
+            return ConstructPath(currentNode);
+        }
+
+        // 방문 처리를 위해 열린 리스트에서 제거
+        for (auto iterator = openList.begin(); iterator != openList.end(); iterator++)
+        {
+            // iterator 위치의 노드가 currentNode 인지 확인
+            if ((*iterator) == currentNode)
+            {
+                openList.erase(iterator);
+                break;
+            }
+        }
+
+        // 현재 노드를 방문 노드에 추가
+        closedList.emplace_back(currentNode);
+
+        // 이웃 노드 방문 (탐색)
+        for (const Direction& direction : directions)
+        {
+            // 다음에 이동할 위치 (이웃 노드의 위치)
+            int newX = currentNode->position.x + direction.x;
+            int newY = currentNode->position.y + direction.y;
+
+            // 유효성 검증 (새 위치가 이동 가능한지 확인)
+            if (!IsInRange(newX, newY, grid))
+            {
+                continue;
+            }
+            
+            // 새 위치가 이동 가능한 곳인지 확인
+            // 장애물 (못 가는곳) = 1
+            if (grid[newY][newX] == 1)
+            {
+                continue;
+            }
+            
+            // 현재 노드를 기준으로 gCost 계산
+            float newGCost = currentNode->gCost + direction.cost;
+
+            // 갈 수는 있지만, 이미 방문한 곳인지 확인
+            if (HasVisited(newX, newY, newGCost))
+            {
+                continue;
+            }
+
+            // 방문을 위한 이웃 노드 생성
+            Node* neighborNode = new Node(newX, newY, currentNode);
+            // 비용 계산
+            neighborNode->gCost = newGCost;
+            neighborNode->hCost = CalculateHeuristic(neighborNode, this->goalNode);
+            neighborNode->fCost = neighborNode->gCost + neighborNode->hCost;
+
+            // 이웃 노드가 열린 리스트에 있는지 확인
+            Node* openListNode = nullptr;
+            for (Node* const node : openList)
+            {
+                // 위치만 비교해서 열린 리스트에 넣을지 여부 확인
+                if (*node == *neighborNode)
+                {
+                    openListNode = node;
+                    break;
+                }
+            }
+
+            // 이웃 노드가 열린 리스트에 있으면 더 좋은 비용일 때만 처리
+            if (openListNode)
+            {
+                // 비용 확인
+                if (neighborNode->gCost < openListNode->gCost ||
+                    neighborNode->fCost < openListNode->fCost)
+                {
+                    // 부모 노드 업데이트
+                    openListNode->parentNode = currentNode;
+                    // 비용 업데이트
+                    openListNode->gCost = neighborNode->gCost;
+                    openListNode->fCost = neighborNode->fCost;
+                    openListNode->hCost = neighborNode->hCost;
+                }
+
+                // 임시 노드 메모리 정리
+                SafeDelete(neighborNode);
+                continue;
+            }
+
+            // 방문할 목록에 추가
+            // 이 노드가 이동 가능한지 확인
+            // 이동 가능한 곳 = 0
+            if (grid[newY][newX] == 0)
+            {
+                // 시각화를 위해 사용 안하는 값 정해서 설정
+                grid[newY][newX] = 5;
+            }
+
+            // 열린 리스트에 추가
+            openList.emplace_back(neighborNode);
+
+            // 잠시 대기 (옵션)
+            DisplayGrid(grid);
+            DWORD delay = static_cast<DWORD>(0.05f * 1000);
+            Sleep(delay);
+        }
+    }
+
+    return {};
 }
 
 void AStar::DisplayGridWithPath(std::vector<std::vector<int>>& grid, const std::vector<Node*>& path)
@@ -184,7 +338,7 @@ bool AStar::HasVisited(int x, int y, float gCost)
     for (Node* const node : closedList)
     {
         if (node->position.x == x && node->position.y == y
-            && gCost >= node->gCost)
+            /*&& gCost >= node->gCost*/)
         {
             return true;
         }
